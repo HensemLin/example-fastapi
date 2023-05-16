d641ada6b1323dbd1ee8a77470c35edd4cc12eb8
HensemLin <jiayuanlin838@gmail.com>
2023-05-16 13:41:28 +0800

    added first action

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -14,12 +14,12 @@ def vote(vote: schemas.Vote, db: Session = Depends(database.get_db), current_use
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with {vote.post_id} does not exist")
 
-    vote_query = db.query(models.votes).filter(models.votes.post_id == vote.post_id, models.votes.user_id == current_user.id)
+    vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id)
     found_vote = vote_query.first()
     if (vote.dir==1):
         if found_vote:
             raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"user {current_user.id} has already voted on post {vote.post_id}")
-        new_vote = models.votes(post_id = vote.post_id, user_id = current_user.id)
+        new_vote = models.Vote(post_id = vote.post_id, user_id = current_user.id)
         db.add(new_vote)
         db.commit()
         return {"message": "successfully added vote"}
